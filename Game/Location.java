6e91570c07b1a4922b57f38d60cd13580ac4dbf2
matsudc <matsudc@wwu.edu>
2020-05-08 17:51:04 -0700

    board still broken. need fix

--- Game/Location.java
+++ Game/Location.java
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-import Board;
-=======
-import Game.Player;
-import Game.Board;
-import Game.Scene;
-import Game.Deadwood;
+package Game;
 
->>>>>>> 127684a4f09102ebc088625524025bfc4ae11a06
+
+//import Game.Player;
+//import Game.Board;
+//import Game.Scene;
+//import Game.Deadwood;
+
 
 public class Location{
 
@@ -16,26 +15,27 @@ int[] role;
 int[] moves;
 String name;
 
-  public static void Location(int index, String name, int[] moves){
+ public Location(int index, String name, int[] moves){
     this.index = index;
     this.name = name;
-    this.moves = moves
+    this.moves = moves;
   }
 
-  public static void Location(int index, String name, int[] moves, int[] roles, int shot){
+
+  public Location(int index, String name, int[] moves, int[] roles, int shot){
     this.index = index;
     this.name = name;
-    this.moves = moves
+    this.moves = moves;
     this.shot = shot;
-    this.roles = roles;
+    this.role = roles;
   }
 
   public int[] getRoles(){
-    return this.roles;
+    return this.role;
   }
 
   public void updateRoles(int index){
-    this.roles[index] = -1,
+    this.role[index] = -1;
   }
 
   public void updateShot(){
--- Game/Scene.java
+++ Game/Scene.java
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-import Board;
-=======
-import Game.Player;
-import Game.Board;
-import Game.Location;
-import Game.Deadwood;
->>>>>>> 127684a4f09102ebc088625524025bfc4ae11a06
+package Game;
+
+//import Game.Player;
+//import Game.Board;
+//import Game.Location;
+//import Game.Deadwood;
+
 
 public class Scene{
 
